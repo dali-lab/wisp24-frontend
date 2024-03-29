fc1ae4382591066c8520a4f2e02ecfd102d80eaa
Ellie Huang <elliehuang@MacBook-Air-5.local>
2024-03-28 21:35:28 -0400

    started course component

---
+++ src/components/courseComponent/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+
+// course component
+const courseComponent = (props) => (
+    const [name, setName] = useState([]);
+    const [distrib, setDistrib] = useState ([]);
+    const [delete, setDelete] = useState(false);
+    
+
+
+
+
+  <div>blah blah</div>
+);
+
+export default courseComponent;
