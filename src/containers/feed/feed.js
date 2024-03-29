05900b20947c128ed0cc8692d138a6e28f2551c1
Joyce Zou <jzou771@gmail.com>
2024-03-28 21:17:41 -0400

    test

---
+++ src/containers/feed/feed.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Feed = ()=>{
+    useState[likes,setLike] = useState(0); // like bar
+
+
+    return(
+        <div>
+
+        </div>
+    )
+
+
+
+}
+export default Feed;
