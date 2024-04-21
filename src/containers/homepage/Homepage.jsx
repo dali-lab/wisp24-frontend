53e59e2e8b4f329a55cdf8d7827a888a50e97631
Ellie Huang <elliehuang@MacBook-Air-5.local>
2024-04-21 01:35:52 -0400

    course add works, uses user input for course name and NRO and rest is hard coded

--- src/containers/homepage/Homepage.jsx
+++ src/containers/homepage/Homepage.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Plan from '../../components/plan/Plan.jsx';
 import PotentialClass from './PotentialClass.jsx';
 import AddTerms from './AddTerms.jsx';
-import './Homepage.css'
+import './Homepage.css';
 import ProgressTracker from './ProgressTracker.jsx';
+import { addNewCourse } from '../../services/datastore.js';
+// import { signOut } from 'firebase/auth';
 
 const Homepage = () => {
   const [mainDrafts, setMainDrafts] = useState([
@@ -12,8 +14,26 @@ const Homepage = () => {
     }
   ]);
   const inputRef = useRef();
-  const [editingIndex, setEditingIndex] = useState("");
+  const [editingIndex, setEditingIndex] = useState('');
   const [mainDraftIndex, setMainDraftIndex] = useState(0);
+  const [courseName, setCourseName] = useState('');
+  const [courseID, setCourseID] = useState(0); // is it an issue that this resets if u reload the page?
+  const [NRO, setNRO] = useState(false);
+
+  const newCourseName = (event) => {
+    // console.log('course typing registered');
+    setCourseName(event.target.value);
+  };
+
+  const createCourse = () => {
+    // console.log('create course button click registered');
+    addNewCourse(courseID, courseName, 'NW', NRO, 'CS1', 'orange');
+    setCourseID(courseID + 1);
+  };
+
+  const changeNRO = () => {
+    setNRO(!NRO);
+  };
 
   const addDraft = (event) => {
     event.preventDefault();
@@ -22,53 +42,54 @@ const Homepage = () => {
 
   const selectMainDraft = (index) => {
     setMainDraftIndex(index);
-  }
+  };
 
   const deleteDraft = (index) => {
     if (mainDrafts.length > 1) {
       if (editingIndex === index) {
-        setEditingIndex("")
+        setEditingIndex('');
       }
       const updatedDrafts = mainDrafts.filter((draft, i) => i !== index);
-      setMainDrafts(updatedDrafts)
+      setMainDrafts(updatedDrafts);
     }
-  }
+  };
 
   const editDraft = (index) => {
     setEditingIndex(index);
-  }
+  };
 
   const startEdit = (index) => {
-    setEditingIndex(index)
-  }
+    setEditingIndex(index);
+  };
 
   const titleChangeSubmit = (index) => {
-    if (inputRef.current.value.length === 0) {return}
+    if (inputRef.current.value.length === 0) { return; }
     const updatedDrafts = mainDrafts.map((mainDraft, i) => {
       if (index == i) {
-        return {draftTitle: inputRef.current.value}
+        return { draftTitle: inputRef.current.value };
       } else {
-        return mainDraft
+        return mainDraft;
       }
-    })
+    });
     setMainDrafts(updatedDrafts);
     console.log(updatedDrafts);
-    setEditingIndex("");
-  }
+    setEditingIndex('');
+  };
 
   const MainDraftTab = () => {
     return (
+
       <div className="tab-container">
         {mainDrafts.map((mainDraft, index) => {
           return (
-            <div onClick={() => selectMainDraft(index)} className={`tab-list ${index === mainDraftIndex ? "selectedDraft" : "nonselectedDraft"}`} key={index}>
-              <div>{editingIndex === index ? <input type="text" ref={inputRef}></input> : mainDraft.draftTitle}</div>
-              {editingIndex === index ? <button onClick={() => titleChangeSubmit(index)}>Submit</button> : <button onClick={() => startEdit(index)}>Edit</button>}
-              <button onClick={() => deleteDraft(index)}>Delete</button>
+            <div onClick={() => selectMainDraft(index)} className={`tab-list ${index === mainDraftIndex ? 'selectedDraft' : 'nonselectedDraft'}`} key={index}>
+              <div>{editingIndex === index ? <input type="text" ref={inputRef} /> : mainDraft.draftTitle}</div>
+              {editingIndex === index ? <button type="submit" onClick={() => titleChangeSubmit(index)}>Submit</button> : <button onClick={() => startEdit(index)}>Edit</button>}
+              <button type="submit" onClick={() => deleteDraft(index)}>Delete</button>
             </div>
           );
         })}
-        <button className="add-tab-button" onClick={addDraft}>Add Draft</button>
+        <button type="submit" className="add-tab-button" onClick={addDraft}>Add Draft</button>
       </div>
     );
   };
@@ -76,8 +97,15 @@ const Homepage = () => {
   return (
     <div className="homepage-main-container">
       <div className="homepage-left-container">
+
+        <div>
+          <input type="text" placeholder="Course Name" value={courseName} onChange={newCourseName} />
+          <button type="submit" onClick={changeNRO}>{NRO ? 'Remove NRO' : 'Set NRO'}</button>
+          <button type="submit" onClick={createCourse}>Create Course</button>
+        </div>
+
+        {/* <button onClick={addNewCourse} type='submit'>ADD COURSE (test)</button> */}
         <MainDraftTab />
-        <p>testing -ellie</p>
         <div className="plan-container">
           <ProgressTracker />
           <Plan />
