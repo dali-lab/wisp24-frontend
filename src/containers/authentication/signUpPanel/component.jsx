4c7eef64922305d56a5763e98d7475b557c3ba0d
Tasnim Chowdhury <tasnimchowdhurynyc@gmail.com>
2024-05-08 20:46:59 -0400

    adding to auth

--- src/containers/authentication/signUpPanel/component.jsx
+++ src/containers/authentication/signUpPanel/component.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { GoogleLogin } from 'react-oauth/google';
 import { useNavigate } from 'react-router-dom';
+import { getAuth, GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
+import { addUser } from '../../../services/datastore';
 
 const SignUpPanel = () => {
   const navigate = useNavigate();
-  const handleSuccess = (response) => {
-    console.log('Login Success:', response);
-    navigate('/home');
+  const auth = getAuth();
+
+  const handleSuccess = async (response) => {
+    const credential = GoogleAuthProvider.credential(response.credential);
+    try {
+      const result = await signInWithCredential(auth, credential);
+      const { user } = result;
+
+      if (result.additionalUserInfo.isNewUser) {
+        const newUser = {
+          id: user.uid,
+          name: user.displayName,
+          email: user.email,
+          profilePic: user.photoURL,
+        };
+
+        await addUser(user.uid, newUser);
+        console.log('New user added:', user.displayName);
+      }
+
+      console.log('Login Success:', user.displayName);
+      navigate('/home');
+    } catch (error) {
+      console.error('Firebase auth error:', error);
+    }
   };
 
   const handleError = (error) => {
@@ -15,8 +39,11 @@ const SignUpPanel = () => {
 
   return (
     <div>
-      {/* Your existing signup form */}
-      <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+      <GoogleLogin
+        onSuccess={handleSuccess}
+        onError={handleError}
+        useOneTap
+      />
     </div>
   );
 };
